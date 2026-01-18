0b013ff709a2189c1b3e2e031fa8b42c900a33e1
Shivam Patel <shivampatel722490@gmail.com>
2026-01-19 02:14:05 +0530

    google and github auth setup

--- src/components/modals/SignInModal.tsx
+++ src/components/modals/SignInModal.tsx
@@ -3,8 +3,23 @@ import { FaGithub } from "react-icons/fa"
 import Modal from "./Modal"
 import { useModalStore } from "@/store/useModalStore"
 import { FcGoogle } from "react-icons/fc"
+import { authClient } from "@/lib/auth-client"
 
 const SignInModal = () => {
+
+  const signInWithGoogle = async ()=>{
+    await authClient.signIn.social({
+      provider: "google"
+    })
+  }
+
+  const signInWithGithub = async ()=>{
+    await authClient.signIn.social({
+      provider: "github"
+    })
+  }
+
+
     const {isSignInOpen, closeSignIn} = useModalStore()
   return (
     <Modal onClose={closeSignIn} isOpen={isSignInOpen}>
@@ -13,8 +28,9 @@ const SignInModal = () => {
 
       <div className="space-y-4">
         {/* Google */}
-        <button className="w-full flex items-center justify-center gap-3 py-3 rounded-full cursor-pointer bg-white text-black font-medium hover:bg-gray-200 transition  "> <FcGoogle className="text-xl" /> Continue With Google</button>
-        <button className="w-full flex items-center justify-center gap-3 py-3 rounded-full cursor-pointer bg-hover text-white border border-white/10  font-medium hover:bg-[#202020] transition  "> <FaGithub className="text-xl"/>  Continue With Github</button>
+        <button onClick={signInWithGoogle} className="w-full flex items-center justify-center gap-3 py-3 rounded-full cursor-pointer bg-white text-black font-medium hover:bg-gray-200 transition  "> <FcGoogle className="text-xl" /> Continue With Google</button>
+        {/* Github */}
+        <button onClick={signInWithGithub} className="w-full flex items-center justify-center gap-3 py-3 rounded-full cursor-pointer bg-hover text-white border border-white/10  font-medium hover:bg-[#202020] transition  "> <FaGithub className="text-xl"/>  Continue With Github</button>
 
       </div>
 
